dd1e8791dfdb200b384815fe31e3d803cd4b009a
NAS6mixfoolv <nas6@nas6.net>
2025-05-24 17:51:06 +0900

    Update solarsystem.js

--- javascripts/nas6/solarsystem.js
+++ javascripts/nas6/solarsystem.js
@@ -256,14 +256,9 @@ function viewp() {
   var selid = F1.VP.selectedIndex;
   var elm = document.getElementById('viewp000');
 
-  //transform view vector and view point to local mirror space
-  //var viewMatInv = x3domRuntime.viewMatrix().inverse();
-  //var vPos = viewMatInv.multMatrixPnt(new x3dom.fields.SFVec3f(0, 0, 0));
-
   var SWM = x3domRuntime.viewMatrix().inverse(); //ワールド回転行列取得
-  var WM = new N6LMatrix().FromX3DOM(SWM);
+  var WM = new N6LMatrix().FromX3DOM(SWM).TransposedMat();
   var Seye = SWM.multMatrixPnt(new x3dom.fields.SFVec3f(0, 0, 0)); //視点位置取得
-//  var Seye = currentPosition; //視点位置取得
   var sp = new x3dom.fields.SFVec3f(mp[selid].x.x[1] / CNST_AU / Zoom, -mp[selid].x.x[0] / CNST_AU / Zoom, mp[selid].x.x[2] / CNST_AU / Zoom);
   var Sat = x3dom.fields.SFVec3f.copy(sp);
   var lookat = new N6LVector([1.0, Sat.x, Sat.y, Sat.z], true);
