ab48050d7d2c264a3b3d7ddd4d37b0c86c2573a3
NAS6mixfoolv <nas6@nas6.net>
2025-05-31 19:02:50 +0900

    Add files via upload

--- javascripts/nas6lib/prime.js
+++ javascripts/nas6lib/prime.js
@@ -1,11 +1,13 @@
 ﻿//Programed by NAS6
 //prime.js
 
-const N6LONELOOPNUM = 1000;
-
-var N6LNUM;
-var N6LMAX;
+var N6LISPRMONELOOPNUM = 1000;
+var N6LISPRMNUM;
+var N6LISPRMMAX;
 var N6LISPRMRET = 0;
+var N6LISPRMTMP = 5;
+var N6LISPRMTMP0 = 5;
+var N6LISPRMTMP1 = 5;
 
 //入力////////////////////////////////////////////////
 //id  : TManのid
@@ -15,52 +17,53 @@ var N6LISPRMRET = 0;
 //-1  : 素数でない
 //0   : 計算継続中
 //////////////////////////////////////////////////////
-function N6LIsPrime(id, num){
+function N6LIsPrime(id, num, loopnum){
   N6LISPRMRET = 0;
+  N6LISPRMONELOOPNUM = loopnum;
 
-  N6LNUM = num;
-  N6LMAX = Math.ceil(Math.sqrt(NUM));
+  N6LISPRMNUM = num;
+  N6LISPRMMAX = Math.ceil(Math.sqrt(N6LISPRMNUM));
 
-  if((N6LNUM == 1)||(N6LNUM == 2)||(N6LNUM == 3)){
-    N6LISPRMRET = N6LNUM;
-    return N6LNUM;
+  if((N6LISPRMNUM == 1)||(N6LISPRMNUM == 2)||(N6LISPRMNUM == 3)){
+    N6LISPRMRET = N6LISPRMNUM;
+    return N6LISPRMNUM;
   }
-  if((N6LNUM <= 0)||(N6LNUM % 2 == 0)){
+  if((N6LISPRMNUM <= 0)||(N6LISPRMNUM % 2 == 0)){
     N6LISPRMRET = -1;
     return -1;
   }
 
-  N6LTMP = 5;
+  N6LISPRMTMP = 5;
 
-  var mod = N6LNUM % 6;
-  if((N6LNUM != 3)&&((mod != 1)&&(mod != 5))){
+  var mod = N6LISPRMNUM % 6;
+  if((N6LISPRMNUM != 3)&&((mod != 1)&&(mod != 5))){
     N6LISPRMRET = -1;
     return -1;
   }
 
-  if(N6LMAX <= N6LTMP){
-    N6LISPRMRET = N6LNUM;
-    return N6LNUM;
+  if(N6LISPRMMAX <= N6LISPRMTMP){
+    N6LISPRMRET = N6LISPRMNUM;
+    return N6LISPRMNUM;
   }
 
   function Loop(id){
     var cnt;
-    for(cnt = 0; cnt < N6LONELOOPNUM; cnt++){
-      mod = N6LTMP % 6;
+    for(cnt = 0; cnt < N6LISPRMONELOOPNUM; cnt++){
+      mod = N6LISPRMTMP % 6;
       if((mod != 1)&&(mod != 5)){
-         N6LTMP += 2;
+         N6LISPRMTMP += 2;
         continue;
       }
-      if(N6LNUM % N6LTMP == 0){
+      if(N6LISPRMNUM % N6LISPRMTMP == 0){
         N6LISPRMRET = -1;
         return -1;
       }
 
-      N6LTMP += 2;
+      N6LISPRMTMP += 2;
 
-      if(N6LMAX <= N6LTMP){
-         N6LISPRMRET = N6LNUM;
-         return N6LNUM;
+      if(N6LISPRMMAX <= N6LISPRMTMP){
+         N6LISPRMRET = N6LISPRMNUM;
+         return N6LISPRMNUM;
       }
     }
 
@@ -75,58 +78,59 @@ function N6LIsPrime(id, num){
 
 
 
-function N6LIsPrimeMNRD(id, num){
+function N6LIsPrimeMNRD(id, num, loopnum){
   N6LISPRMRET = 0;
+  N6LISPRMONELOOPNUM = loopnum;
 
-  N6LNUM = num;
-  N6LMAX = Math.ceil(Math.sqrt(NUM));
+  N6LISPRMNUM = num;
+  N6LISPRMMAX = Math.ceil(Math.sqrt(N6LISPRMNUM));
 
-  if((N6LNUM == 1)||(N6LNUM == 2)||(N6LNUM == 3)){
+  if((N6LISPRMNUM == 1)||(N6LISPRMNUM == 2)||(N6LISPRMNUM == 3)){
     N6LISPRMRET = N6LNUM;
     return N6LNUM;
   }
-  if((N6LNUM <= 0)||(N6LNUM % 2 == 0)){
+  if((N6LISPRMNUM <= 0)||(N6LISPRMNUM % 2 == 0)){
     N6LISPRMRET = -1;
     return -1;
   }
 
-  N6LTMP0 = Math.ceil(N6LMAX / 2) * 2 + 1;
-  N6LTMP1 = 5;
+  N6LISPRMTMP0 = Math.ceil(N6LISPRMMAX / 2) * 2 + 1;
+  N6LISPRMTMP1 = 5;
 
-  var mod = N6LNUM % 6;
-  if((N6LNUM != 3)&&((mod != 1)&&(mod != 5))){
+  var mod = N6LISPRMNUM % 6;
+  if((N6LISPRMNUM != 3)&&((mod != 1)&&(mod != 5))){
     N6LISPRMRET = -1;
     return -1;
   }
 
-  if(N6LTMP0 <= N6LTMP1){
-    N6LISPRMRET = N6LNUM;
-    return N6LNUM;
+  if(N6LISPRMTMP0 <= N6LISPRMTMP1){
+    N6LISPRMRET = N6LISPRMNUM;
+    return N6LISPRMNUM;
   }
 
   function LoopMNRD(id){
     var cnt;
-    for(cnt = 0; cnt < N6LONELOOPNUM; cnt++){
-      mod = N6LNUM % N6LTMP0;
+    for(cnt = 0; cnt < N6LISPRMONELOOPNUM; cnt++){
+      mod = N6LISPRMNUM % N6LISPRMTMP0;
       if(mod != 0){
-        N6LTMP0 -= 2;
+        N6LISPRMTMP0 -= 2;
       }
       else{
         N6LISPRMRET = -1;
         return -1;
       }
-      mod = N6LNUM % N6LTMP1;
+      mod = N6LISPRMNUM % N6LISPRMTMP1;
       if(mod != 0){
-        N6LTMP1 += 2;
+        N6LISPRMTMP1 += 2;
       }
       else{
         N6LISPRMRET = -1;
         return -1;
       }
 
-      if(N6LTMP0 <= N6LTMP1){
-        N6LISPRMRET = N6LNUM;
-        return N6LNUM;
+      if(N6LISPRMTMP0 <= N6LISPRMTMP1){
+        N6LISPRMRET = N6LISPRMNUM;
+        return N6LISPRMNUM;
       }
     }
 
